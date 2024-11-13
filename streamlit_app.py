7ab430662619339e7ea00c89be3f6c34005b7883
おってぃ <otty0507@gmail.com>
2024-11-13 17:29:48 +0000

    xac

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ from streamlit_gsheets import GSheetsConnection
 
 # @st.cache(suppress_st_warning=True)
 def main():
-    params = st.experimental_get_query_params()
-    st.text(params)
-    id=params.get('id')
+    id = st.query_params.id
+    # st.text(params)
+    # id=params.get('id')
     st.text(id)
     st.text('https://docs.google.com/spreadsheets/d/'+str(id)+'/edit?gid=0#gid=0')
     url = 'https://docs.google.com/spreadsheets/d/'+str(id)+'/edit?gid=0#gid=0'
