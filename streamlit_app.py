681b5b60c95eb25f90f511acb8bb33a6bfc6c181
おってぃ <otty0507@gmail.com>
2024-11-13 16:18:33 +0000

    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
-import streamlit as st
+# example/st_app.py
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# @st.cache(suppress_st_warning=True)
+def main():
+    params = st.experimental_get_query_params()
+    st.text(params)
+    # url = "https://docs.google.com/open?id=1ZGyumlD4vl6AN4EcIUIYeDK5w_48qF2LUONQqC1KJ0E"
+
+    # conn = st.connection("gsheets", type=GSheetsConnection)
+
+    # data = conn.read(spreadsheet=url, usecols=[0, 1])
+    # st.dataframe(data)
+main()
